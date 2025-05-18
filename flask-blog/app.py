f1bd39441866811a8a99e42b009baf1aab4d86a6
huyhoang-ph <phungh67@gmail.com>
2025-05-18 21:00:35 +0700

    feat: intergate database to web app

--- flask-blog/app.py
+++ flask-blog/app.py
@@ -1,25 +1,68 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
 
+import os
+
+
+# Boostrapping appliation with pre-definition
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SECRET_KEY'] = 'thisisasecretstring'
+
+# Configuration of database connection
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+# Definition of Form for submit
 class NameForm(FlaskForm):
     name = StringField("Input your name in this field?", validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
-app.config['SECRET_KEY'] = 'thisisasecretstring'
+# Definition of database models
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique = True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r' % self.name
+    
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r' % self.username
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['know'] = False
+        else:
+            session['know'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), know=session.get('know', False))
 
 @app.route('/user/<name>')
 def user(name):
@@ -33,6 +76,11 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+# Handle shell
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
 
 if __name__ == '__main__':
     app.run()
