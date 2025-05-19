0ea3391eebe12668274edde3cd075a4149151ac8
huyhoang-ph <phungh67@gmail.com>
2025-05-19 07:58:17 +0700

    feat: mail server - untested

--- flask-blog/app.py
+++ flask-blog/app.py
@@ -4,13 +4,15 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_mail import Mail
 
 import os
-
 
 # Boostrapping appliation with pre-definition
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+mail = Mail(app)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -22,6 +24,15 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+# Migrate database
+migrate = Migrate(app, db)
+
+# Apply mail server
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 
 # Definition of Form for submit
 class NameForm(FlaskForm):
