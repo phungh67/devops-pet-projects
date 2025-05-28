bc90a8f14f521bfe2d9bf866adddfc1e79f043b5
huyhoang-ph <phungh67@gmail.com>
2025-05-28 21:47:06 +0700

    feat: template and form

--- flask-mega/app/routes.py
+++ flask-mega/app/routes.py
@@ -1,6 +1,24 @@
+from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = {'username': 'Hoang'}
+    posts = [
+        {
+            'author': {'username': 'Hoang'},
+            'body': 'Beautiful day in Sweden'
+        },
+        {
+            'author': {'username': 'Skjord'},
+            'body': 'Something to remember at MidSummer fest'
+        }
+    ]
+    return render_template('index.html', title = 'Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
