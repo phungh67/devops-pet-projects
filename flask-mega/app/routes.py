9afeae0ae0493dc8c32276762f19eeb87c1d6a5d
huyhoang-ph <phungh67@gmail.com>
2025-06-03 19:40:48 +0700

    feat: implement user registration

--- flask-mega/app/routes.py
+++ flask-mega/app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template, flash, redirect, url_for
-from flask_login import current_user, login_user, logout_user
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
+from urllib.parse import urlsplit
 from app import app
 from app import db
 from app.forms import LoginForm
@@ -9,6 +10,7 @@ import sqlalchemy as sa
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Hoang'}
     posts = [
@@ -21,7 +23,7 @@ def index():
             'body': 'Something to remember at MidSummer fest'
         }
     ]
-    return render_template('index.html', title = 'Home', user=user, posts=posts)
+    return render_template('index.html', title = 'Home', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -35,7 +37,10 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
