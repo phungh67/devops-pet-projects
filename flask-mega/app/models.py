0dd4a5d1d17746cb9fa98797592f1b5d59a85b78
huyhoang-ph <phungh67@gmail.com>
2025-06-11 22:23:47 +0700

    feat: user's avatar

--- flask-mega/app/models.py
+++ flask-mega/app/models.py
@@ -1,6 +1,7 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timezone
 from typing import Optional
+from hashlib import md5
 from flask_login import UserMixin
 import sqlalchemy as sa
 import sqlalchemy.orm as so
@@ -19,6 +20,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
