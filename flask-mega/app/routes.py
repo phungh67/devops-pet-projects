d0aa4c38c625c6105762a392a18f7d29e5524718
huyhoang-ph <phungh67@gmail.com>
2025-06-24 23:09:14 +0700

    feat: pagination function

--- flask-mega/app/routes.py
+++ flask-mega/app/routes.py
@@ -142,14 +142,18 @@ def index():
         db.session.commit()
         flash('You post is now live!')
         return redirect(url_for('index'))
-    posts = db.session.scalars(current_user.following_posts()).all()
-    return render_template('index.html', title='Home Page', form=form, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = db.paginate(current_user.following_posts(), page=page,
+                        per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    return render_template('index.html', title='Home Page', form=form, posts=posts.items)
 
 @app.route('/explore')
 def explore():
+    page = request.args.get('page', 1, type=int)
     query = sa.select(Post).order_by(Post.timestamp.desc())
-    posts = db.session.scalars(query).all()
-    return render_template('index.html', title = 'Explore', posts=posts)
+    posts = db.paginate(query, page=page,
+                        per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    return render_template('index.html', title = 'Explore', posts=posts.items)
 
 @app.before_request
 def before_request():
