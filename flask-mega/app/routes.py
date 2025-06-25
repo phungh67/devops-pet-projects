5bacb2233e1066aba9fdb0ced5cf8aa086e9e012
huyhoang-ph <phungh67@gmail.com>
2025-06-25 21:31:51 +0700

    bugs: not display correct navigation on index page

--- flask-mega/app/routes.py
+++ flask-mega/app/routes.py
@@ -145,7 +145,7 @@ def index():
     page = request.args.get('page', 1, type=int)
     posts = db.paginate(current_user.following_posts(), page=page,
                         per_page=app.config['POSTS_PER_PAGE'], error_out=False)
-    next_url = url_for('index', page=posts.prev_num) \
+    next_url = url_for('index', page=posts.next_num) \
         if posts.has_next else None
     prev_url = url_for('index', page=posts.prev_num) \
         if posts.has_prev else None
