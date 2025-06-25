b60de27dbc23d46edf98e8f118a995ae21f4b2aa
huyhoang-ph <phungh67@gmail.com>
2025-06-25 21:28:37 +0700

    feat: navigation for older, newer posts

--- flask-mega/app/routes.py
+++ flask-mega/app/routes.py
@@ -145,7 +145,12 @@ def index():
     page = request.args.get('page', 1, type=int)
     posts = db.paginate(current_user.following_posts(), page=page,
                         per_page=app.config['POSTS_PER_PAGE'], error_out=False)
-    return render_template('index.html', title='Home Page', form=form, posts=posts.items)
+    next_url = url_for('index', page=posts.prev_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Home Page', form=form, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
 
 @app.route('/explore')
 def explore():
@@ -153,7 +158,12 @@ def explore():
     query = sa.select(Post).order_by(Post.timestamp.desc())
     posts = db.paginate(query, page=page,
                         per_page=app.config['POSTS_PER_PAGE'], error_out=False)
-    return render_template('index.html', title = 'Explore', posts=posts.items)
+    next_url = url_for('explore', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title = 'Explore', posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
 
 @app.before_request
 def before_request():
