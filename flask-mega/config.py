9c9e4ff9a9b48890f2920efa50a903bb5a27e23f
huyhoang-ph <phungh67@gmail.com>
2025-07-04 23:19:50 +0700

    feat: migration from sqlite3 to MySQL

--- flask-mega/config.py
+++ flask-mega/config.py
@@ -3,7 +3,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-know'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Root2025%40@localhost/app'
     POSTS_PER_PAGE = 3
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
