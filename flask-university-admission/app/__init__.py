d93ed039e00c877091107d9bf650d086177569b2
huyhoang.ph <phungh67@gmail.com>
2025-07-17 21:38:18 +0700

    add: CSS and base template

--- flask-university-admission/app/__init__.py
+++ flask-university-admission/app/__init__.py
@@ -1,5 +1,17 @@
 from flask import Flask
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-from app import routes
+    # Register routes
+    from .routes.home import home_bp
+    # from .routes.scholarships import scholarships_bp
+    # from .routes.admissions import admissions_bp
+    # from .routes.checklist import checklist_bp
+
+    app.register_blueprint(home_bp)
+    # app.register_blueprint(scholarships_bp, url_prefix="/scholarships")
+    # app.register_blueprint(admissions_bp, url_prefix="/admissions")
+    # app.register_blueprint(checklist_bp, url_prefix="/checklist")
+
+    return app
--- flask-university-admission/app/routes/home.py
+++ flask-university-admission/app/routes/home.py
@@ -1,6 +1,8 @@
-from app import app
+from flask import Blueprint, render_template
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Hello Worlds!", 200
+home_bp = Blueprint("home", __name__)
+
+@home_bp.route("/")
+def home():
+    return render_template('home.html', title='Home')
+    # return render_template("home.html")
