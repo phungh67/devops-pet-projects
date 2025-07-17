29cc3688a0afc0eae0befd8a4496422a44e90242
huyhoang.ph <phungh67@gmail.com>
2025-07-17 22:00:23 +0700

    tweak: revert to old template

--- flask-university-admission/app/routes/home.py
+++ flask-university-admission/app/routes/home.py
@@ -3,6 +3,7 @@ from flask import Blueprint, render_template
 home_bp = Blueprint("home", __name__)
 
 @home_bp.route("/")
+@home_bp.route("/home")
 def home():
     return render_template('home.html', title='Home')
     # return render_template("home.html")
