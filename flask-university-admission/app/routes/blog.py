e267f054d57f7a74b46e80dd1ddb7c0440555575
huyhoang.ph <phungh67@gmail.com>
2025-09-05 21:02:36 +0200

    bugs: minor bugs in blog route

--- flask-university-admission/app/routes/blog.py
+++ flask-university-admission/app/routes/blog.py
@@ -27,10 +27,10 @@ def blog_list_view():
     posts = BlogPost.query.order_by(BlogPost.created_at.desc()).all()
     return render_template("blog/list.html", posts=posts)
 
-@blog_bp.route("/", methods=["GET"])
-def list_posts():
-    posts = BlogPost.query.order_by(BlogPost.created_at.desc()).all()
-    return render_template("blog.html", posts=posts)
+# @blog_bp.route("/", methods=["GET"])
+# def list_posts():
+#     posts = BlogPost.query.order_by(BlogPost.created_at.desc()).all()
+#     return render_template("blog.html", posts=posts)
 
 @blog_bp.route("/<int:post_id>", methods=["GET"])
 def view_post(post_id):
